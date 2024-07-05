3d1c33fcc42c4f17a200100a270d46e791fd0d68
Jose Escobar <jgescobarquintero@gmail.com>
2024-07-05 17:50:34 +0200

    modificacion 05 07

--- ejercicio008_funciones_y_metodos/validations.js
+++ ejercicio008_funciones_y_metodos/validations.js
@@ -1,11 +1,19 @@
 function emailValidation(email, password) {
+
+    emailError.textContent = '';
+    passwordError.textContent = '';
     if (email.indexOf("@") >= 0 & email.length > 5 & email.indexOf(".") >= 0) {
-        if (password.toLowerCase().indexOf("a") >= 0 | password.toLowerCase().includes("IBM") != true | password.length > 6) {
-            alert("Error en password")
+        errorMessage("", 'emailError');
+        if (password.toLowerCase().indexOf("a") >= 0 | password.toLowerCase().includes("IBM") != true | password.length < 6) {
+            errorMessage("", 'passwordError');
         } else {
-            alert("Correcto");
+            errorMessage("Error en Password", 'passwordError');
         }
-    } else {
-        alert("Error en email");
+    } else {  
+        errorMessage("Error en Email", 'emailError');
     }
+}
+
+function errorMessage (message, selector) {
+    document.querySelector(`#${selector}`).innerHTML = message;
 }

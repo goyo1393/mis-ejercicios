3a50771dbf27bb336a93416d333b014f9f7bedd4
Jose Escobar <jgescobarquintero@gmail.com>
2024-07-12 17:40:24 +0200

    Actualizacion champs

--- ejercicio015_gestor_destinos/funciones.js
+++ ejercicio015_gestor_destinos/funciones.js
@@ -1,6 +1,15 @@
 
 const championPull = new Array();
 
+//Recuperar de localStorage los campeones almacenados
+let champStorage = localStorage.getItem("championPull");
+if (champStorage!=null)
+{
+    champs = JSON.parse(champStorage);
+    createChampionPull(champs);
+}
+//Fin de recuperar de localStorage
+
 document.querySelector("#aButton").addEventListener("click", (event) => {
     let name = document.querySelector('#iChampion').value;
     let line = document.querySelector('#iLane').value; 
@@ -18,16 +27,26 @@ document.querySelector("#aButton").addEventListener("click", (event) => {
 function createChampionPull(championPull) {
     let htmlChamp = "";
     championPull.map((champ) => {
-        htmlChamp += 
-        `
-            <div>${champ.name}</div>
-            <div>${champ.line}</div>
-            <img src="${champ.image}">
+        htmlChamp += `
+            <div class="card">
+                <div class="card-name"><strong>${champ.name}</strong></div>
+                <div class="card-line"><strong>${champ.line}</strong></div>
+                <img src="${champ.image}" alt="${champ.name}">
+            </div>
         `;
     });
-    document.querySelector("#cardContainer").innerHTML=htmlChamp;
+    document.querySelector("#cardContainer").innerHTML = htmlChamp;
 
-    document.querySelector("#iChampion").value="";
-    document.querySelector("#iLane").value="";
-    document.querySelector("#iImage").value="";
+    document.querySelector("#iChampion").value = "";
+    document.querySelector("#iLane").value = "";
+    document.querySelector("#iImage").value = "";
 }
+
+//Guardar los destinos en localStorage
+document.querySelector("#gButton").addEventListener("click",guardar);
+function guardar() {
+    console.log("guardar")
+    let strChamps = JSON.stringify(champs);
+    localStorage.setItem("championPull", strChamps);
+}
+//Fin de Guardar destinos

66e1fc2b420739a87a8974777e5208e09cd2ced7
Jose Escobar <jgescobarquintero@gmail.com>
2024-07-23 18:13:54 +0200

    modificacion sin servidor

--- ejercicio024_DOM/funciones.js
+++ ejercicio024_DOM/funciones.js
@@ -2,10 +2,11 @@ const backend = "http://localhost:3000/";
 let dogosArray;
 const dButton = document.querySelector("#dButton");
 const buscador = document.querySelector("#buscador");
+const getDogsUrl = "https://api.thedogapi.com/v1/images/search?limit=60&has_breeds=true";
+const APK = "live_wSZ4qJqqdQCA0yeMOwMV4hDMyGUBgr7Eq2dUlp3TRY6BlkY3Q1wkRqOOJpXbXp07";
 
 const createCard = (temperament, img, race) => {
 
-    // Crea elemento div de carta inicial <div class="card"/>
     let divCard = document.createElement("div");
     divCard.classList.add("card");
     document.querySelector("#dogos").appendChild(divCard);
@@ -25,7 +26,11 @@ const createCard = (temperament, img, race) => {
 
 const cardGenerator = async () => {
     try {
-        const response = await fetch(backend);
+        const response = await fetch(getDogsUrl, {
+            headers: {
+                "x-api-key": APK,
+            },
+        });
         dogosArray = await response.json();
         dogosArray.forEach(dog => {
             createCard(dog.breeds[0].temperament, dog.url, dog.breeds[0].name);
